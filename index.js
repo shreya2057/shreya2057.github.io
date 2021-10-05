3c59735b3230506be1df22e0279e4f2eb3b186b1
Shreya Shrestha <shreya2057@gmail.com>
2021-10-05 15:52:14 +0545

    Adding icon to the title of the website

--- index.js
+++
@@ -1,4 +0,0 @@
-function buttonClicked(){
-     alert("I am Shreya Shrestha");
-     document.write('<div style="color:blue; padding-top: 200px;text-align:center; font-size:50px">Thank You!!!</div>')
-}
