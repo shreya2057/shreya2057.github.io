4e1401dbf75e0e061006da72a346a19563ca65e8
Shreya Shrestha <shreya2057@gmail.com>
2021-10-05 15:41:51 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function buttonClicked(){
+     alert("I am Shreya Shrestha");
+     document.write('<div style="color:blue; padding-top: 200px;text-align:center; font-size:50px">Thank You!!!</div>')
+}
