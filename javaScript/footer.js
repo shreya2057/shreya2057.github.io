ccbbe49cefbfa9b681562480b348fc1939f3dbda
shreya2057 <shreya2057@gmail.com>
2022-12-02 15:35:42 +0545

    Footer created

---
+++ javaScript/footer.js
@@ -0,0 +1,30 @@
+document.write(`
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <footer class="bg-dark text-light">
+      <div class="container text-light p-4 ps-sm-0">
+        <div class="row">
+          <div class="col-3">
+            <img src="./images/logo.jpg" class="img-fluid" width="150px">
+          </div>
+          <div class="col-9 text-center align-self-center">
+            <div class="container fs-4 m-2">
+              <a class="pe-3 text-light"><i class="bi bi-linkedin"></i></a>
+              <a class="pe-3 text-light"><i class="bi bi-facebook"></i></a>
+              <a class="pe-3 text-light"><i class="bi bi-twitter"></i></a>
+              <a class="pe-3 text-light"><i class="bi bi-github"></i></a>
+              <a class="pe-3 text-light><i class="bi bi-youtube"></i></a>
+            </div>
+            <div class="container m-2">
+              <a href="./index.html" class="pe-3 text-decoration-none text-light">Home</a>
+              <a href="./html/about.html" class="pe-3 text-decoration-none text-light">About</a>
+              <a href="./html/projects.html" class="pe-3 text-decoration-none text-light">Projects</a>
+              <a href="./html/certificates.html" class="pe-3 text-decoration-none text-light">Certificates</a>
+              <a href="./html/resume.html" class="pe-3 text-decoration-none text-light">Resume</a>
+              <a href="./html/contact.html" class="pe-3 text-decoration-none text-light">Contact</a>
+            </div>
+            <i class="bi bi-globe"></i><a class="ps-3 text-decoration-none text-light" href="https://shreya2057.github.io/">https://shreya2057.github.io/</a>
+          </div>
+        </div>
+      </div>
+     </footer>
+`)
