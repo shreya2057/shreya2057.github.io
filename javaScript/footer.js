57b229ceefa459a617e60237c952caf3aec5fd39
shreya2057 <shreya2057@gmail.com>
2022-12-05 19:20:49 +0545

    changes

--- javaScript/footer.js
+++ javaScript/footer.js
@@ -3,7 +3,7 @@ document.write(`
       <div class="container text-light p-4 ps-sm-0">
         <div class="row">
           <div class="col-3">
-            <img src="../images/myLogo/logo.jpg" class="img-fluid" width="150px">
+            <img src="../images/mylogo/logo.jpg" class="img-fluid" width="150px">
           </div>
           <div class="col-9 text-center align-self-center">
             <div class="container fs-4 m-2">
